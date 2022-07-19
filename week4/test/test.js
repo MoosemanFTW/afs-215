e624fbbd77dfbd76ab277a0ea6632850f295d4c7
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2022-07-19 16:41:52 -0700

    Save Point

--- week4/test/test.js
+++ week4/test/test.js
@@ -41,3 +41,83 @@ it('expect answer = 6', function(){
 it('expect answer = 1.5', function(){
     expect(calculator(3,2,'div')).to.equal(1.5)
 })
+
+
+function HandleMultiples(arg){
+    if(arg % 3 == 0 && arg % 7 == 0){
+        let newArg = 37
+        return newArg
+    }
+    else if(arg % 3 == 0){
+        let newArg = 3
+        return newArg
+    }
+    else if(arg % 7 == 0){
+        let newArg = 7
+        return newArg
+    }
+    else if(typeof arg != typeof NaN){
+        let newArg = 1
+        return newArg
+    }
+    else{
+        let newArg = 0
+        return newArg
+    }
+}
+
+function returnStuff(arg){
+    switch(HandleMultiples(arg)){
+        case 0:
+            let StrNum = arg
+            let ResultingString = StrNum.toString()
+            return ResultingString
+        case 1:
+            return ('Please enter in a number')
+        case 3:
+            return ('Good Morning');
+        case 7:
+            return ('Good Afternoon')
+        case 37:
+            return ('Good Evening')
+        default:
+            return ('Something went wrong')
+    }
+    }
+    
+    
+    it('ReturnStuff tests', function(){
+        expect(returnStuff(1)).to.equal(1)
+    })
+    
+    it('expect "1"', function(){
+        expect(returnStuff(1)).to.equal('1')
+    })
+
+    it('expect "2"', function(){
+        expect(returnStuff(2)).to.equal('2')
+    })
+
+    it('expect "Good Morning"', function(){
+        expect(returnStuff(3)).to.equal('Good Morning')
+    })
+
+    it('expect "Good Morning" when given multi of 3', function(){
+        expect(returnStuff(6)).to.equal('Good Morning')
+    })
+
+    it('expect "Good Afternoon"', function(){
+        expect(returnStuff(7)).to.equal('Good Afternoon')
+    })
+
+    it('expect "Good Afternoon" when given a multi of 7', function(){
+        expect(returnStuff(14)).to.equal('Good Afternoon')
+    })
+    
+    it('expect "Good Evening" when given a multi of 3 and 7', function(){
+        expect(returnStuff(21)).to.equal('Good Evening')
+    })
+
+    it('expect an error message', function(){
+        expect(returnStuff('this is not a number')).to.equal('Please enter in a number')
+    })
